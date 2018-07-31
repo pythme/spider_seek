c2ac5e9a662bdfdfa443f61f20758f70285c4a5b
pythme <pythme@gmail.com>
2018-08-01 01:16:22 +0800

    update

--- ArticleSpider/ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -5,7 +5,9 @@ import scrapy
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/114167/']
 
     def parse(self, response):
+        # //*[@id = "post-114167"]/div[1]/h1
+        re_selector = response.xpath("//div/h1/text()")
         pass
--- ArticleSpider/main.py
+++ ArticleSpider/main.py
@@ -2,9 +2,10 @@
 __author__ = 'pythme'
 
 from scrapy.cmdline import execute
-import sys
+# import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 execute(["scrapy", "crawl", "jobbole"])
 # after go into spider project directory,ready to debug this main.py document.
