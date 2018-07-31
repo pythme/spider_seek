e6b2bf1b49a312be89ce605e330b26f17bc556cc
pythme <pythme@gmail.com>
2018-07-31 20:03:58 +0800

    update

---
+++ ArticleSpider/main.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+__author__ = 'pythme'
+
+from scrapy.cmdline import execute
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+execute(["scrapy", "crawl", "jobbole"])
+# after go into spider project directory,ready to debug this main.py document.
